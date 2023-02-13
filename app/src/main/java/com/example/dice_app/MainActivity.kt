0a57634345818e2ba4e3e0eb7e91e6a3ef18dca3
wasingun <wasingun00@gmail.com>
2023-02-14 00:48:50 +0900

    주사위 완성 (숫자 입력시 입력범위 구현)

--- app/src/main/java/com/example/dice_app/MainActivity.kt
+++ app/src/main/java/com/example/dice_app/MainActivity.kt
@@ -3,19 +3,26 @@ package com.example.dice_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        val min: EditText = findViewById(R.id.minNum)
+        val max: EditText = findViewById(R.id.maxNum)
         val numberSet: TextView = findViewById(R.id.diceNum)
         val dice: Button = findViewById(R.id.rolling)
         dice.setOnClickListener {
-            val number = (1..6).random()
-            numberSet.text = number.toString()
+            val min2: Int = Integer.parseInt(min.text.toString())
+            val max2: Int = Integer.parseInt(max.text.toString())
+            if (min2 > max2){
+                numberSet.text = "실행할 수 없습니다."
+            } else {
+                val number = (min2..max2).random()
+                numberSet.text = number.toString()
+            }
         }
     }
 }
